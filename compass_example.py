6c9ad3f87e8c5e91700031980e9125c0ba0ffab4
Jay Allison <jay.allison@synapsewireless.com>
2018-07-20 13:27:39 -0500

    Adding my own compass example, plus a requirements.txt for this project

--- compass_example.py
+++ compass_example.py
@@ -1,24 +1,16 @@
 # derived from https://github.com/adafruit/Adafruit_Python_LSM303/blob/master/examples/simpletest.py
 
+import math
 import time
 
 # sudo pip install adafruit-lsm303
 import Adafruit_LSM303
 
-# Create a LSM303 instance.
+# device is connected on I2C Bus 0
 lsm303 = Adafruit_LSM303.LSM303()
 
-# Alternatively you can specify the I2C bus with a bus parameter:
-#lsm303 = Adafruit_LSM303.LSM303(busum=2)
-
-print('Printing accelerometer & magnetometer X, Y, Z axis values, press Ctrl-C to quit...')
 while True:
-    # Read the X, Y, Z axis acceleration values and print them.
-    accel, mag = lsm303.read()
-    # Grab the X, Y, Z components from the reading and print them out.
-    accel_x, accel_y, accel_z = accel
-    mag_x, mag_z, mag_y = mag
-    print('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={5}'.format(
-          accel_x, accel_y, accel_z, mag_x, mag_y, mag_z))
-    # Wait half a second and repeat.
+    accelerometer, magnetometer = lsm303.read()
+    compass_heading = math.atan2(magnetometer[1], magnetometer[0]) * 180 / math.pi
+    print "Compass heading for " + str(compass_heading) + ": " + str(compass_heading)
     time.sleep(0.5)
