0e091a7baf0dc8a5fc1b1c61f836ca515936f121
Jay Allison <jay.allison@synapsewireless.com>
2018-07-26 11:32:24 -0500

    Tweaks to display example, fix PEP errors, etc.

--- display_example.py
+++ display_example.py
@@ -2,7 +2,6 @@
 # dependencies: sudo apt-get install build-essential python-dev python-smbus python-pip python-imaging python-numpy
 # for me, only python-imaging was not already installed
 
-# ST7735 Display Library: https://github.com/cskau/Python_ST7735
 # I got the cheap Amazon display I purchased working with the examples using this pinout:
 #   * 5V - any RPi 5V
 #   * GND - any RPi GND
@@ -17,28 +16,31 @@
 #   * CS - RPi CE0
 # The examples are written for a 160x128 display, this one is 128x128
 
-from PIL import Image
-from PIL import ImageDraw
+# from PIL import Image
+# from PIL import ImageDraw
 from PIL import ImageFont
 
+# need to install this from source: https://github.com/cskau/Python_ST7735
 import ST7735
-import Adafruit_GPIO as GPIO
+# import Adafruit_GPIO as GPIO
 import Adafruit_GPIO.SPI as SPI
 
 # TFT display parameters
 TFT_WIDTH = 128
 TFT_HEIGHT = 128
-SPEED_HZ = 4000000
+#SPEED_HZ = 4000000
+# TODO: try a faster speed and see how well it works
+SPEED_HZ = 15000000
 
 # RPi GPIO & SPI configuration for TFT display
-DC = 24
+RS = 24
 RST = 25
 SPI_PORT = 0
 SPI_DEVICE = 0
 
 # Create TFT LCD display driver
 disp = ST7735.ST7735(
-    DC,
+    RS,
     rst=RST,
     spi=SPI.SpiDev(
         SPI_PORT,
@@ -49,7 +51,7 @@ disp = ST7735.ST7735(
 
 # Initialize display, clear the buffer to white, write buffer to screen
 disp.begin()
-disp.clear((0,0,0))
+disp.clear((0, 0, 0))
 disp.display()
 draw = disp.draw()
 
@@ -58,8 +60,8 @@ text = "Hello, World!"
 small_font = ImageFont.truetype("Perfect DOS VGA 437 Win.ttf", 8)
 medium_font = ImageFont.truetype("Perfect DOS VGA 437 Win.ttf", 16)
 big_font = ImageFont.truetype("Perfect DOS VGA 437 Win.ttf", 32)
-draw.text((10, 10), text, font=small_font, fill=(255,0,0))
-draw.text((10, 30), text, font=medium_font, fill=(0,255,0))
-draw.text((10, 60), text, font=big_font, fill=(0,0,255))
+draw.text((10, 10), text, font=small_font, fill=(255, 0, 0))
+draw.text((10, 30), text, font=medium_font, fill=(0, 255, 0))
+draw.text((10, 60), text, font=big_font, fill=(0, 0, 255))
 
 disp.display()
