dbea26edb51dbc1f93ebdaf4e2df396d133578a3
Jay Allison <jay.allison@synapsewireless.com>
2018-09-13 17:35:51 -0500

    Starting to build the menu-based UI...

--- UserInterface.py
+++ UserInterface.py
@@ -1,4 +1,5 @@
 from gpiozero import Button
+from PIL import ImageFont
 from signal import pause
 
 class UserInterface(object):
@@ -14,13 +15,18 @@ class UserInterface(object):
                  right_button_gpio):
 
         self.display = display
-        self.top_menu = top_menu
+        self.current_menu = top_menu
+
         self.error_led = error_led
+
         self.top_button = Button(top_button_gpio)
         self.center_button = Button(center_button_gpio)
         self.bottom_button = Button(bottom_button_gpio)
         self.left_button = Button(left_button_gpio)
         self.right_button = Button(right_button_gpio)
+
+        self.font = ImageFont.truetype("../Fonts/Perfect DOS VGA 437 Win.ttf", 16)
+        self.menu_color = (255, 255, 255)
 
     def run(self):
         self.top_button.when_pressed = self.top_pressed
@@ -29,21 +35,48 @@ class UserInterface(object):
         self.left_button.when_pressed = self.left_pressed
         self.right_button.when_pressed = self.right_pressed
 
+        # show the menu to the user
+        self.show_menu(self.current_menu)
+
         # now wait for the user's actions
         pause()
 
+    def show_menu(self, menu):
+        canvas = self.display.draw()
+        canvas.text((10, 10), menu.title, font=self.font, fill=self.menu_color)
+        self.display.display()
 
     def top_pressed(self):
         print "TOP"
 
+        canvas = self.display.draw()
+        canvas.text((10, 10), "TOP", font=self.font, fill=self.menu_color)
+        self.display.display()
+
     def center_pressed(self):
         print "CENTER"
+
+        canvas = self.display.draw()
+        canvas.text((10, 10), "CENTER", font=self.font, fill=self.menu_color)
+        self.display.display()
 
     def bottom_pressed(self):
         print "BOTTOOM"
 
+        canvas = self.display.draw()
+        canvas.text((10, 10), "BOTTOOM", font=self.font, fill=self.menu_color)
+        self.display.display()
+
     def left_pressed(self):
         print "LEFT"
 
+        canvas = self.display.draw()
+        canvas.text((10, 10), "LEFT", font=self.font, fill=self.menu_color)
+        self.display.display()
+
     def right_pressed(self):
         print "RIGHT"
+
+        canvas = self.display.draw()
+        canvas.text((10, 10), "RIGHT", font=self.font, fill=self.menu_color)
+        self.display.display()
